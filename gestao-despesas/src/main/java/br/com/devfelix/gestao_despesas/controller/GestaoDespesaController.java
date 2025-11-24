772ac5d881d4cc00d1509eb67938b6c2cbbca022
Dariofelix84 <dariofa69@gmail.com>
2025-11-24 16:43:36 -0300

    Imprementação da Resposta "Preecha todos os campos" do meu GestaoDespesaController

--- gestao-despesas/src/main/java/br/com/devfelix/gestao_despesas/controller/GestaoDespesaController.java
+++ gestao-despesas/src/main/java/br/com/devfelix/gestao_despesas/controller/GestaoDespesaController.java
@@ -3,6 +3,7 @@ package br.com.devfelix.gestao_despesas.controller;
 import br.com.devfelix.gestao_despesas.entity.Despesa;
 import br.com.devfelix.gestao_despesas.service.CadastroDespesaService;
 import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,11 +14,14 @@ public class GestaoDespesaController {
     public GestaoDespesaController(CadastroDespesaService cadastroDespesaService) {
         this.cadastroDespesaService = cadastroDespesaService;
     }
-
     @PostMapping("/create")
-    public Despesa create(@RequestBody Despesa despesa) {
-        return cadastroDespesaService.execute(despesa);
+    public ResponseEntity<?> create(@RequestBody Despesa despesa) {
+
+        try {
+            var result =  cadastroDespesaService.execute(despesa);
+            return ResponseEntity.ok(result);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(400).body(e.getMessage());
+        }
     }
-
-
 }
--- gestao-despesas/src/main/java/br/com/devfelix/gestao_despesas/service/CadastroDespesaService.java
+++ gestao-despesas/src/main/java/br/com/devfelix/gestao_despesas/service/CadastroDespesaService.java
@@ -1,9 +1,7 @@
 package br.com.devfelix.gestao_despesas.service;
-
 import br.com.devfelix.gestao_despesas.entity.Despesa;
 import br.com.devfelix.gestao_despesas.repository.DespesaRepository;
 import org.springframework.stereotype.Service;
-
 @Service
 public class CadastroDespesaService {
 
@@ -16,7 +14,7 @@ public class CadastroDespesaService {
         if (despesa.getDescricao() == null || despesa.getCategoria() == null ||
                 despesa.getValor() == null || despesa.getEmail() == null) {
 
-            throw new IllegalArgumentException("Preencha todos os campos");
+            throw new IllegalArgumentException("Preencha todos os campos!");
         }
         despesa = despesaRepository.save(despesa);
         return despesa;
